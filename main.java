b1c3a69c7503947b97453ac342704f543196c23d
isajan <68370085+isajan@users.noreply.github.com>
2023-11-05 11:55:51 +0100

    Update main.java

--- main.java
+++ main.java
@@ -3,4 +3,7 @@ public static void main(String[] args){
   System.out.PrintLn("Hoj hoj jag är i github");
   System.out.PrintLn("Hoj hoj jag är i github");
   System.out.PrintLn("Hoj hoj jag är i github");
+  System.out.PrintLn("Hoj hoj jag är i github");
+  System.out.PrintLn("Hoj hoj jag är i github");
+  System.out.PrintLn("Hoj hoj jag är i github");  
 }
