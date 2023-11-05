587730b60d96a099f5f70dc478fbfe535a64795d
Isa Janfada <isa.janfada@gmail.com>
2023-11-05 12:20:37 +0100

    uppgift1

--- main.java
+++ main.java
@@ -6,4 +6,10 @@ public static void main(String[] args){
   System.out.PrintLn("Hoj hoj jag är i github");
   System.out.PrintLn("Hoj hoj jag är i github");
   System.out.PrintLn("Hoj hoj jag är i github");  
+    System.out.PrintLn("Hoj hoj jag är i github uppgift1");
+  System.out.PrintLn("Hoj hoj jag är i github");
+  System.out.PrintLn("Hoj hoj jag är i github");
+  System.out.PrintLn("Hoj hoj jag är i github");
+  System.out.PrintLn("Hoj hoj jag är i github");
+  System.out.PrintLn("Hoj hoj jag är i github uppgift1");  
 }
